70109259e0edfdb788b7ff424f3d241f5501493c
Thiago Yukio Kikuchi Oliveira <stratust@gmail.com>
2014-03-17 11:50:33 -0400

    Added script to refine breaks

--- bin/TC_Seq.pl
+++ bin/TC_Seq.pl
@@ -103,6 +103,107 @@ class MyApp::Process_Primers {
     }
 }
 
+class MyApp::RefineBreakpoints {
+    extends 'MyApp'; # inherit log
+    use MooseX::App::Command;    # important
+    use MooseX::FileAttribute;
+    use IO::Uncompress::AnyUncompress qw(anyuncompress $AnyUncompressError);
+    use Data::Printer;
+ 
+    command_short_description q[Merge breakpoints based on shear end];
+
+    has_file 'input_file' => (
+        traits        => ['AppOption'],
+        cmd_type      => 'option',
+        cmd_aliases   => [qw(i)],
+        must_exist    => 1,
+        required      => 1,
+        documentation => q[Breakpoints bed file],
+    );
+
+    option 'output_dir' => (
+          is            => 'rw',
+          isa           => 'Str',
+          required      => '1',
+          cmd_aliases   => [qw(o)],
+          default => 'results',
+          documentation => q['Output directory'],
+    );
+
+    has_file 'output_file' => (
+        traits        => ['AppOption'],
+        cmd_type      => 'option',
+        cmd_aliases   => [qw(f)],
+        must_exist    => 0,
+        required      => 1,
+        default => 'targets_refined.bed',
+        documentation => q[Output bed file],
+    );
+
+    method get_clusters_by_shear {
+        my $in = IO::Uncompress::AnyUncompress->new($self->input_file->stringify) 
+           or die "Cannot open: $AnyUncompressError\n";
+        
+        my %clusters;
+        my %cluster_totals;
+        while ( my $row = <$in> ) {
+            chomp $row;
+            my @F = split "\t", $row;
+            my $id = $F[3];
+            my $n_reads = $F[4];
+
+            #chr15_61818338_+|chr8_61779084_+_0-left_(chr8_61779763)
+            if ( $id =~ /^(chr.*)\|(chr.*)_(.*)_\((.*)\)/ ) {
+                my ( $bait, $bait_target, $primer, $target ) =
+                  ( $1, $2, $3, $4 );
+
+                #say join "\t", ( $bait, $bait_target, $primer, $target );
+                my $p = 'right';
+                $p = 'left' if $primer =~ /left/;
+                my $strand = '+';
+                $strand = '-' if $bait_target =~ /\-$/;
+                my $shear_key = "$target|$p|$strand";
+                push @{ $clusters{$shear_key}{$n_reads} }, $row;
+                $cluster_totals{$shear_key} += $n_reads;
+            }
+            else {
+                $self->log->warn( 'Cannot find 3 positions in:' . $id );
+            }
+        }
+        close( $in );
+        
+        return ( \%clusters, \%cluster_totals );
+
+    }
+
+
+    method refine_breakpoints (HashRef $clusters, HashRef $total) {
+        foreach my $shear_key ( sort { $a cmp $b } keys %{$clusters} ) {
+            foreach  my $n_reads ( sort { $a <=> $b } keys %{ $clusters->{$shear_key} } )
+            {
+                my $row = shift @{ $clusters->{$shear_key}->{$n_reads} };
+                my @F = split "\t", $row;
+                $F[4] = $total->{$shear_key};
+                say join "\t", @F;
+                last;
+            }
+        }
+    }
+
+    method run {        
+        my $cmd;
+        $cmd = $1 if __PACKAGE__ =~ /\:\:(.*)$/;        
+        $self->log->warn("==> Starting $cmd <==");
+        # Code Here
+        my ( $clusters, $totals) = $self->get_clusters_by_shear();
+        $self->refine_breakpoints( $clusters, $totals );
+        
+        $self->log->warn("==> END $cmd <==");
+    }
+}
+
+   
+
 class Main {
     import MyApp;
     MyApp->new_with_command->run();
